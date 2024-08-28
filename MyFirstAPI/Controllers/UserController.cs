cf73156a8802ecd45ad86ef2105206e8f7123cc4
João <Joo@DESKTOP-9V72JTK>
2024-08-27 22:44:13 -0300

    Add project files.

---
+++ MyFirstAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //Primeiro endpoint
+        [HttpGet]
+        public IActionResult GetResult()
+        {
+            var response = new Response
+            {
+                Age = 22,
+                Name = "João Moraes"
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ MyFirstAPI/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstAPI
+{
+    public class Response
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
