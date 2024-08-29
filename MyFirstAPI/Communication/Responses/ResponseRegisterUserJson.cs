2547822c0e9cb09251d73aef9dfa7f35077df1cc
joohxz <joohxz@gmail.com>
2024-08-29 00:44:26 -0300

    Create a Post Request

---
+++ MyFirstAPI/Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstAPI.Communication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- MyFirstAPI/Controllers/UserController.cs
+++ MyFirstAPI/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Communication.Requests;
+using MyFirstAPI.Communication.Responses;
 
 namespace MyFirstAPI.Controllers
 {
@@ -13,7 +16,7 @@ namespace MyFirstAPI.Controllers
         //Path Param
         //[Route("{Id}/person/{nickname}")]
         [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
-        public IActionResult GetResult([FromQuery] int Id, [FromQuery] string? nickname)
+        public IActionResult GetByID([FromQuery] int Id, [FromQuery] string? nickname)
         {
             var response = new User
             {
@@ -22,6 +25,18 @@ namespace MyFirstAPI.Controllers
                 Name = "João Moraes"
             };
             return Ok(response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult CreateUser([FromBody] RequestRegisterUserJson request)
+        {
+            var response = new ResponseRegisterUserJson
+            {
+                Id = 1,
+                Name = request.Name,
+            };
+            return Created(string.Empty, response);
         }
     }
 }
