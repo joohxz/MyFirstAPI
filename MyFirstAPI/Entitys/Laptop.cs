5441171904c5f15e341244a4dd0abcd71b702ee9
joohxz <joohxz@gmail.com>
2024-09-02 15:20:04 -0300

    Adds Laptop

---
+++ MyFirstAPI/Entitys/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstAPI.Entitys
+{
+    public class Laptop : Device
+    {
+        public string GetModel()
+        {
+            var isConnected = IsConnected();
+            if (isConnected)
+                return "MacBook";
+            return "Unknown";
+        }
+    }
+}
